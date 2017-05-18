e86fc02ca3654fefaf475fd1215aca79787013d5
Fábio Santos <fabiosantosart@gmail.com>
2017-05-18 18:43:52 +0100

    style: make tests prettier

--- test/index.js
+++ test/index.js
@@ -1,61 +1,72 @@
-'use strict';
+'use strict'
 
-var assert = require('assert')
+var ok = require('assert')
 var rel = require('..')
 
 describe('rel', () => {
   before(() => {
     // instrument rel for tests
     rel._configure({
-      createElement: function () { return [].slice.call(arguments) },
+      createElement: (...args) => args,
       guid: 'rel-guid',  // just for console.logs in the middle of the code to not get confusing
     })
   })
   it('returns a createElement structure', () => {
-    assert.deepEqual(rel`
+    ok.deepEqual(rel`
       <div id="foo" />
     `, [ 'div', { id: 'foo' } ])
   })
   it('this structure can interpolate things', () => {
-    assert.deepEqual(rel`<div>${'thing'}</div>`, [ 'div', { }, 'thing' ])
+    ok.deepEqual(rel`<div>${'thing'}</div>`, [ 'div', { }, 'thing' ])
     var fn = () => null
-    assert.deepEqual(rel`<div>${fn}</div>`, [ 'div', { }, fn ])
+    ok.deepEqual(rel`<div>${fn}</div>`, [ 'div', { }, fn ])
+  })
+  it('can interpolate attributes', () => {
+    var fn = () => null
+    ok.deepEqual(
+      rel`<div onClick=${fn} />`,
+      [ 'div', { onClick: fn } ]
+    )
   })
   it('can interpolate parts of strings', () => {
-    assert.deepEqual(rel`<div>${'thing'} is ${'great'}</div>`, [
+    ok.deepEqual(rel`<div>${'thing'} is ${'great'}</div>`, [
       'div',
       { },
       'thing',
       ' is ',
       'great'
     ])
-    assert.deepEqual(rel`<div> ${'thing'} is ${'great'} </div>`, [
+    ok.deepEqual(rel`<div> ${'thing'} is ${'great'} </div>`, [
       'div',
       { },
-      ' ',
-      'thing',
-      ' is ',
-      'great',
-      ' ',
+      ' ', 'thing', ' is ', 'great', ' ',
     ])
-    assert.deepEqual(rel`<div> ${'thing'} is </div>`, [
+    ok.deepEqual(rel`<div> ${'thing'} is </div>`, [
       'div',
       { },
-      ' ',
-      'thing',
-      ' is ',
+      ' ', 'thing', ' is ',
     ])
-    assert.deepEqual(rel`<div> is ${'thing'} </div>`, [
+    ok.deepEqual(rel`<div> is ${'thing'} </div>`, [
       'div',
       { },
-      ' is ',
-      'thing',
-      ' ',
+      ' is ', 'thing', ' ',
     ])
+  })
+  it('removes extraneous whitespace when it contains a \\n', () => {
+    ok.deepEqual(rel`
+      <div>
+        foo
+      </div>
+    `, [ 'div', { }, 'foo' ])
+    ok.deepEqual(rel`
+      <div>
+        ${'foo'}
+      </div>
+    `, [ 'div', { }, 'foo' ])
   })
   it('(actual examples)', () => {
     const lel = 'sloog'
-    assert.deepEqual(rel`<div className="foo">
+    ok.deepEqual(rel`<div className="foo">
       <div className="bar baz">
         <img className="baz-avatar" src=${`/foo/${ lel }/baz-avatar`} />
       </div>
